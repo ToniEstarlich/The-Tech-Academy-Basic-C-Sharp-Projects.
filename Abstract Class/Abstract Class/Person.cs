5421f7497c7ccf78d28f3f18677fd732abf2a0e0
Antonio Estarlich <110102891+ToniEstarlich@users.noreply.github.com>
2023-12-08 20:40:03 +0000

    Add files via upload

---
+++ Abstract Class/Abstract Class/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    //Step 1: Create an abstract class called Person with two properties:
+    public abstract class Person
+    {
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Step 2: Method to say the name
+        public abstract void SayName();
+    }
+
+    // Step 3: Create another class called Employee and have it inherit from the Person class.
+    public class Employee : Person
+    {
+        // Step 4: Implement the SayName() method inside of the EMployee class.
+        public override void SayName()
+        {
+            Console.WriteLine($"Employee's name: {FirstName} {LastName}");
+        }
+    }
---
+++ Abstract Class/Abstract Class/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Abstract_Class
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Step 5: Inside the Main() method, instantiate an Employee object with firstName "Sample" and lastName "Student".
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            // Call the SayName() method on the object
+            employee.SayName();
+
+            // Keep the console window open to see the result.
+            Console.ReadLine();
+        }
+    }
+}
