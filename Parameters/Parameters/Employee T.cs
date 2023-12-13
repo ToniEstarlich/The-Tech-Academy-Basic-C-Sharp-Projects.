a1a21a6cc6497409fffd9cac5a47cf4305e14ef4
Antonio Estarlich <110102891+ToniEstarlich@users.noreply.github.com>
2023-12-13 18:01:36 +0000

    Add files via upload

---
+++ Parameters/Parameters/Employee T.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+    // Step 1 Make the EMployee class take a generic type parameter.
+    internal class Employee<T>
+    {
+    // Step 2: Add a property to the Employee class called "things" and have its data type be a generic list matching the generic type of the class.
+    public List<T> Things { get; set; } = new List<T>();
+
+    // Properties
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Constructor
+
+    public Employee(int id, string firstName, string lastName)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+  }
---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Step 3: Instantiate an Employee object with type “string” as its generic parameter.
+        Employee<string> stringEmployee = new Employee<string>(1, "John", "Doe");
+        stringEmployee.Things = new List<string> { "Thing1", "Thing2", "Thing3" };
+
+        // Step 4: Instantiate an Employee object with type “int” as its generic parameter.
+        Employee<int> intEmployee = new Employee<int>(2, "Jane", "Doe");
+        intEmployee.Things = new List<int> { 1, 2, 3, 4, 5 };
+
+        // Step 5: Create a loop that prints all of the Things to the Console.
+        PrintThings(stringEmployee);
+        PrintThings(intEmployee);
+
+    }
+
+    // Helper method to print Things from an Employee object
+    static void PrintThings<T>(Employee<T> employee)
+    {
+        Console.WriteLine($"Employee {employee.Id}: {employee.FirstName} {employee.LastName}");
+
+        // Loop to print all Things
+        foreach (var thing in employee.Things)
+        {
+            Console.WriteLine($"  {thing}");
+        }
+
+        Console.WriteLine();
+    }
+}
