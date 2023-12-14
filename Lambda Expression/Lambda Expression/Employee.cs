805fc57c78fdc7ccfdce95739ed378b661f0f3e7
Antonio Estarlich <110102891+ToniEstarlich@users.noreply.github.com>
2023-12-14 22:00:16 +0000

    Add files via upload

---
+++ Lambda Expression/Lambda Expression/Employee.cs
@@ -0,0 +1,13 @@
+﻿// Employee.cs
+using System;
+
+namespace Lambda_Expression
+{
+    //Create an Employee class with id, firstname and lastname properties
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
