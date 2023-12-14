c6c84da72ff2a535d335f2740841bf1d5acce58d
Antonio Estarlich <110102891+ToniEstarlich@users.noreply.github.com>
2023-12-14 14:52:44 +0000

    Add files via upload

---
+++ Struct/Struct/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    public struct Number
+    {
+      // Property
+      public decimal Amount { get; }
+      
+      // Constructor to initialize the Amount property
+      public Number(decimal amount)
+    {
+        Amount = amount;
+    }
+ }
---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        // Creating an object of the Number struct and assigning an amount using the constructor
+        Number myNumber = new Number(123.45m);
+
+        // Printing the amount to the console
+        Console.WriteLine($"Amount: {myNumber.Amount}");
+
+    }
+  }
+
